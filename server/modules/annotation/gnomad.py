9088198b1749711ea06d79204b8e7cc9af1878d4
Mahesh Vangala <vangalamaheshh@gmail.com>
2018-08-07 11:30:17 -0400

    right merge of variants to gnomad annot

--- server/modules/annotation/gnomad.py
+++ server/modules/annotation/gnomad.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
   variants.columns = ["Chrom", "Position", "Start_Alt", "Reference", "Alternate", "Comments"]
   variants.loc[variants.Reference == '-', "Reference"] = '.'
   variants.loc[variants.Alternate == '-', "Alternate"] = '.'
-  results = pd.merge(gnomad_info, variants, on = ["Chrom", "Position", "Reference", "Alternate"], how = "inner") 
+  results = pd.merge(gnomad_info, variants, on = ["Chrom", "Position", "Reference", "Alternate"], how = "right") 
   results = results[["Chrom", "Position", "Reference", "Alternate", "Protein Consequence", 
     "Transcript Consequence", "Annotation", "Allele Frequency", "RSID", "Comments"]] 
   results.columns = ["Chrom", "Start", "Ref", "Alt", "p.gnomad", "c.gnomad", "func.gnomad", "AF.gnomad", "rs.gnomad", "Comments"]
