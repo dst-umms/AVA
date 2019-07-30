e14b6a18c18fb98fd59db66a3f360262c40a2972
Mahesh Vangala <vangalamaheshh@gmail.com>
2019-07-30 18:34:08 +0000

    added discussed nomenclature to final json

--- server/modules/annotation/gen_json.py
+++ server/modules/annotation/gen_json.py
@@ -12,9 +12,10 @@ import sys
 import json
 import yaml
 
-sources = yaml.safe_load(open("/usr/local/bin/AVA/server/utils/version/sources.yaml"))
+SOURCE_INFO = yaml.safe_load(open("/usr/local/bin/AVA/server/utils/version/sources.yaml"))
+FACT_INFO = yaml.safe_load(open("/usr/local/bin/AVA/server/utils/version/facts.yaml"))
 VERSION_INFO = dict()
-for source_obj in sources["sources"]:
+for source_obj in SOURCE_INFO["sources"]:
   VERSION_INFO[source_obj["SourceName"].lower()] = {
     "VersionName": source_obj["VersionName"],
     "VersionId": source_obj["VersionId"]
@@ -22,26 +23,22 @@ for source_obj in sources["sources"]:
 
 def get_facts(info):
   facts = []
-  sources = ['P.gnomad', 'C.gnomad', 'FUNC.gnomad', 'AF.gnomad', 'RS.gnomad', 'C.ald', 'P.ald', 'EXON.ald', \
-            'REMARK.ald', 'HGVS.clinvar', 'SIG.clinvar', 'MC.clinvar', 'RS.clinvar', 'RS.dbsnp', 'AF.dbsnp', 'P.exac', \
-            'C.exac', 'FUNC.exac', 'AF.exac', 'RS.exac', 'C.emv', 'P.emv', 'SIG.emv', 'REVIEW_DATE.emv',
-            "C.polyphen", "P.polyphen",
-            "FUNC.polyphen", "DSCORE.polyphen", "FUNC_HDIV.polyphen", "PROB_HDIV.polyphen",
-            "FUNC_HVAR.polyphen", "PROB_HVAR.polyphen", "C.pompe", "P.pompe", "FUNC.pompe", "YEAR.pompe", "SOURCE.pompe",
-            "P.mps1", "MTYPE.mps1", "FUNC.mps1", "AUTHOR.mps1", "PAPER.mps1", "P.sift", "FUNC.sift", "RS.sift", "SCORE.sift"                       
-          ]
-  for source in sources:
-    if not info[source] == '-':
-      facts.append({
-        "FactLabel": source
-        , "Name": source.split(".")[0]
-        , "FactValue": info[source]
-        , "FactSource": {
-          "Name": source.split(".")[1]
-          , "VersionId": VERSION_INFO[source.split(".")[1]]["VersionId"]
-          , "VersionName": VERSION_INFO[source.split(".")[1]]["VersionName"]
-        }
-      })
+  for source_obj in SOURCE_INFO["sources"]:
+    source_name = source_obj["SourceName"].lower()
+    for fact_obj in FACT_INFO["facts"]:
+      fact_name = fact_obj["FactName"].upper()
+      col_name = fact_name + '.' + source_name
+      if col_name in info and not info[col_name] == '-':
+        facts.append({
+          "FactLabel": col_name
+          , "Name": fact_name
+          , "FactValue": info[col_name]
+          , "FactSource": {
+            "Name": source_name
+            , "VersionId": VERSION_INFO[source_name]["VersionId"]
+            , "VersionName": VERSION_INFO[source_name]["VersionName"]
+          }
+        })
   return facts
 
 def format_variant(info):
@@ -86,14 +83,14 @@ def format_variant(info):
 
 def main(in_file):
   content = pd.read_csv(in_file, sep = "\t", header = 0)
-  content.columns = ["Chrom", "Start", "Ref", "Alt", "P.gnomad", "C.gnomad", "FUNC.gnomad", "AF.gnomad", "RS.gnomad", "Gene", 
+  content.columns = ["Chrom", "Start", "Ref", "Alt", "P.gnomad", "C.gnomad", "MC.gnomad", "AF.gnomad", "RS.gnomad", "Gene", 
                     "RunID", "SpecID", "C.", "Comments", \
-                    "C.ald", "P.ald", "EXON.ald", "REMARK.ald", "HGVS.clinvar", "SIG.clinvar", "MC.clinvar", "RS.clinvar", "RS.dbsnp", \
-                    "AF.dbsnp", "P.exac", "C.exac", "FUNC.exac", "AF.exac", "RS.exac", "C.emv", "P.emv" \
-  , "SIG.emv", "REVIEW_DATE.emv", "C.polyphen", "P.polyphen",
-    "FUNC.polyphen", "DSCORE.polyphen", "FUNC_HDIV.polyphen", "PROB_HDIV.polyphen",
-    "FUNC_HVAR.polyphen", "PROB_HVAR.polyphen", "C.pompe", "P.pompe", "FUNC.pompe", "YEAR.pompe", "SOURCE.pompe",
-    "P.mps1", "MTYPE.mps1", "FUNC.mps1", "AUTHOR.mps1", "PAPER.mps1", "P.sift", "FUNC.sift", "RS.sift", "SCORE.sift"]
+                    "C.ald", "P.ald", "LOCATION.ald", "FUNC.ald", "HGVS.clinvar", "FUNC.clinvar", "MC.clinvar", "RS.clinvar", "RS.dbsnp", \
+                    "AF.dbsnp", "P.exac", "C.exac", "MC.exac", "AF.exac", "RS.exac", "C.emv", "P.emv" \
+                    , "FUNC.emv", "REVIEW_DATE.emv", "C.polyphen", "P.polyphen",
+                    "FUNC.polyphen", "DSCORE.polyphen", "FUNC_HDIV.polyphen", "PROB_HDIV.polyphen",
+                    "FUNC_HVAR.polyphen", "PROB_HVAR.polyphen", "C.pompe", "P.pompe", "FUNC.pompe", "YEAR.pompe", "SOURCE.pompe",
+                    "P.mps1", "MTYPE.mps1", "FUNC.mps1", "AUTHOR.mps1", "PAPER.mps1", "P.sift", "MC.sift", "RS.sift", "SCORE.sift"]
   content["Chrom"] = content["Chrom"].astype(str)
   variants = list(content.apply(lambda row: format_variant(row), axis = 1))
   print(json.dumps({ 
